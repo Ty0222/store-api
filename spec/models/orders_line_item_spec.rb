f4870b321fe6aebb2455631c84e6e3fdcbd7aea2
Ty Kelly <tkelly8@apple.com>
2020-05-20 12:10:36 -0700

    remove validations preventing orders_line_items from saving

--- spec/models/orders_line_item_spec.rb
+++ spec/models/orders_line_item_spec.rb
@@ -3,6 +3,4 @@ require 'rails_helper'
 RSpec.describe OrdersLineItem, type: :model do
   it { is_expected.to belong_to(:order) }
   it { is_expected.to belong_to(:line_item) }
-  it { is_expected.to validate_presence_of(:order_id) }
-  it { is_expected.to validate_presence_of(:line_item_id) }
 end
