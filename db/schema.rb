19b043c657a212f6226ac9689a6ec6ba8b8c8c8a
Ty Kelly <tkelly8@apple.com>
2020-05-20 11:48:45 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_182801) do
+ActiveRecord::Schema.define(version: 2020_05_20_183936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.string "code"
+    t.integer "quantity", default: 0
+    t.decimal "price", precision: 10, scale: 2, default: "0.0"
+    t.decimal "total", precision: 10, scale: 2, default: "0.0"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "code"
@@ -23,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_05_20_182801) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
 end
